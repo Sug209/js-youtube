4094c4b74200e99df614c0c9870de21814f278f1
Sug209 <159291603+Sug209@users.noreply.github.com>
2024-02-07 17:24:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as a new version 
+
+// alert(2+2) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("sujan")
+
+let name="sujan"       //string datatypes
+let age=17             //number datatypes
+let isLoggedIn=true    //boolean types
+
+
+//number => 2 to the power 53
+//bigint
+//string => either in single quote or double quote
+//boolean=> true/false
+//null=> stand alone value or datatypes. Representation of empty value
+//undefined=> not defined or assigned value
+//symbol=> finding uniqueness
+
+
+//object
+
+console.log(typeof null); // output will be object
+console.log(typeof undefined)// output will be undefined
