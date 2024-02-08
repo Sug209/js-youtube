2049fcff86164759693c5584c6bca1975a4f2a28
Sug209 <159291603+Sug209@users.noreply.github.com>
2024-02-08 16:03:38 +0000

    ConversionOperation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber= Number(score)
 let isLoggedIn= "sujan"
 
 let boolenIsLoggedIn= Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
 
 // 1=> true; 0=> false
 // ""=> false
@@ -23,5 +23,30 @@ console.log(boolenIsLoggedIn);
 let someNumber= 33
 
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations *********************//
+
+let value= 3 
+//console.log(value)
+
+let str1= "sujan"
+let str2= " Ghalan"
+
+let str3= str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "3")
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+let number= 3
+number++
+console.log(number)
