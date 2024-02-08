88bfb1fd25a4e64558f4aa601983d5a863704574
Sug209 <159291603+Sug209@users.noreply.github.com>
2024-02-08 16:04:12 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2<1)
+
+// console.log("2" > 1)
+// console.log(2 > "1")
+// console.log("02" > 1)
+
+// console.log(null < 0)   // Compsrision convert null to a number,treating it as 0. That's why (null < 0) is false
+// console.log(null >= 0)    
+// console.log(null == 0)
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+
+// === Strictly checks
+
+console.log("2" === 2)
+console.log("2" === "2")
+
+// clean is important in coding.
